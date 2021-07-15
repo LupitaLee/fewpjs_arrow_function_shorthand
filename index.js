800c55e6115204a207c08ec2a6fe9ce2599e2c98
Lupita <lupitarivera8899@gmail.com>
2021-07-15 16:16:02 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// functional expression called divide
+let num1 = 2000
+let num2 = 100
+let divide = function(){
+return num1 / num2
+}
+
+// has an arrow function called square
+let x = 2
+let square = (x) => x * x 
+
+// has an arrow function called add
+
+let add = (a, b) => a + b
+
+
+//add arrow function takes two parameters and adds them together
+
+
+// let add = (a, b) => a + b
